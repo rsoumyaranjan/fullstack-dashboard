98d6b7233355de60f248b431d4eab4ab6e8d9df2
rsoumyaranjan <142015815+rsoumyaranjan@users.noreply.github.com>
2024-06-02 19:15:24 +0530

    commit

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/api', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
